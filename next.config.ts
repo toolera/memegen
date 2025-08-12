17d55a0b48dcb7588a4073e3e1f5c7f82d84f5ee
ismat-samadov <ismetsemedov@gmail.com>
2025-08-12 13:16:00 +0400

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['i.imgflip.com'],
+  },
 };
 
 export default nextConfig;
